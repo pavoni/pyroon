9bf2a26e640a38419e6ce6d1c4313ab87f939858
Marcel van der Veldt <m.vanderveldt@outlook.com>
2018-08-15 17:26:36 +0200

    some changes for pypi

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
-# Used this guide to create module
-# http://peterdowns.com/posts/first-time-with-pypi.html
-
-# git tag 0.1 -m "0.1 release"
-# git push --tags origin master
-#
 # Upload to PyPI Live
-# python setup.py register -r pypi
-# python setup.py sdist upload -r pypi
+# sudo python3 setup.py sdist bdist_wheel
+# sudo python3 -m twine upload dist/*
+
+import setuptools
 
 VERSION = "0.0.1"
 NAME = "roonapi"
 INSTALL_REQUIRES = ["websocket-client"]
 
-from distutils.core import setup
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name=NAME,
-    packages=['roon'],
     version=VERSION,
-    description='Provides a python interface to interact with Roon',
-    long_description=open("README.md").read(),
     author='Marcel van der Veldt',
     author_email='marcelveldt@users.noreply.github.com',
-    url='https://github.com/marcelveldt/python-roon',
-    download_url = 'https://github.com/marcelveldt/python-roon.git',
-    keywords= ['roon', 'roon labs', 'roon python', 'roon api'],
-    classifiers = [],
+    description='Provides a python interface to interact with Roon',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/marcelveldt/python-roon.git',
+    packages=['roon'],
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ),
     package_data = {'': ['.soodmsg'] },
     install_requires=INSTALL_REQUIRES,
     )
