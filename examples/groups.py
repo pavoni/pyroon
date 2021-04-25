e869e533316adf1ccec10f5eda585560952f9752
pavoni <mail@gregdowling.com>
2021-04-25 17:03:44 +0100

    Rework group code.

--- examples/groups.py
+++ examples/groups.py
@@ -28,22 +28,15 @@ for (k, v) in outputs.items():
     display_name = v["display_name"]
     is_group_main = roonapi.is_group_main(output_id)
     is_grouped = roonapi.is_grouped(output_id)
-    group_main_zone = roonapi.group_main_zone(output_id)
-    group_main_zone_name = roonapi.group_main_zone_name(output_id)
-    if group_main_zone is None:
-        group_main_zone_name_grouped = ""
-    else:
-        group_main_zone_name_grouped = zones[group_main_zone]["display_name"]
+    grouped_zone_names = roonapi.grouped_zone_names(output_id)
     print(
         display_name,
         "grouped?",
         is_grouped,
         "is_main?",
         is_group_main,
-        "main zone:",
-        group_main_zone_name,
-        "group zone name:",
-        group_main_zone_name_grouped,
+        "grouped_zone_names:",
+        grouped_zone_names,
     )
 
 # save the token for next time
