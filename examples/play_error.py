a88ff471cfc0fee6f998e2c7625fcc6fda2724f8
pavoni <mail@gregdowling.com>
2021-05-14 11:56:47 +0100

    Add example to test play error.

---
+++ examples/play_error.py
@@ -0,0 +1,39 @@
+from roonapi import RoonApi
+
+appinfo = {
+    "extension_id": "python_roon_test",
+    "display_name": "Python library for Roon",
+    "display_version": "1.0.0",
+    "publisher": "gregd",
+    "email": "mygreat@emailaddress.com",
+}
+
+server = "192.168.3.60"
+target_zone = "Mixing Speakers"
+# Can be None if you don't yet have a token
+token = open("mytokenfile").read()
+
+roonapi = RoonApi(appinfo, token, server)
+
+# get target zone output_id
+zones = roonapi.zones
+output_id = [
+    output["zone_id"]
+    for output in zones.values()
+    if output["display_name"] == target_zone
+][0]
+print("OUTPUT ID", output_id)
+
+# Examples of using play_media
+
+print("PLAY Something unplayable - should give error")
+items = roonapi.play_media(output_id, ["Qobuz", "My Qobuz", "Favorite Albums"])
+
+print("PLAY Something playable - this should work")
+items = roonapi.play_media(
+    output_id, ["Qobuz", "My Qobuz", "Favorite Albums", "Grover Live"]
+)
+
+# save the token for next time
+with open("mytokenfile", "w") as f:
+    f.write(roonapi.token)
